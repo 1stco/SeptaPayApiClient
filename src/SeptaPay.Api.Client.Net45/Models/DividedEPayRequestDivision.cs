892b7ff32257d48f706eeb72425571706dad6b9c
iman nemati <nemati@gmail.com>
2019-10-31 14:55:57 +0330

    Add InvoiceNumber for Divisions

--- src/SeptaPay.Api.Client.Net45/Models/DividedEPayRequestDivision.cs
+++ src/SeptaPay.Api.Client.Net45/Models/DividedEPayRequestDivision.cs
@@ -3,5 +3,6 @@
         public string ApiKey { get; set; }
         public decimal Amount { get; set; }
         public decimal DividerAmount { get; set; }
+        public string InvoiceNumber { get; set; }
     }
 }
